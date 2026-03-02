5cd2cd9ba76d99905335c0a9b99132e5614b6c97
gg666777228 <98387848+gg666777228@users.noreply.github.com>
2026-03-02 17:37:49 +0500

    add divide

--- test_main.py
+++ test_main.py
@@ -17,3 +17,13 @@ def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+def test_divide():
+    response = client.get("/divide?a=10&b=2")
+    assert response.status_code == 200
+    assert response.json() == {"result": 5.0}
+
+def test_divide_by_zero():
+    response = client.get("/divide?a=10&b=0")
+    assert response.status_code == 200
+    assert response.json() == {"error": "Division by zero is not allowed"}
